d7bd745657597a4b6f4e0551a3f299662edbac31
Lala-banners <lala.l.banno@hotmail.com>
2021-02-09 11:33:11 +1100

    Movement works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Movement
+    public Rigidbody2D rb2D;
+    private float moveSpeed = 20f;
+    private float horizontal;
+    private float vertical;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ShipFlight();
+    }
+
+    /// <summary>
+    /// Player movement 
+    /// </summary>
+    public void ShipFlight()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        rb2D.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
     }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
+    #region UI Elements
+    public TMP_Text titleText;
+    public TMP_Text scoreText;
+    public TMP_Text gameOverText;
+    public Button startGameButton;
+    #endregion
+
+    #region UI Variables
+    private float currentHighScore;
+    public float score;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHighScore = 0f;
+        score = 0f;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void StartGame()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void WinGame()
+    {
+
     }
 }
